4d30f5074b321efcb48d7041d8234805cea25d3b
HassanCHKIRROU <chkirrou.hassan@gmail.com>
2024-10-22 10:24:59 +0100

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,63 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	
-	private static int rashCount = 0;		
-	private static int pupilCount = 0;		
+  
+	//les attributs pour stocker les instances de ISymptomReader et ISymptomWriter
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	
-		int headCount = 0;	// counts headache
-		while (line != null) {
-			i++;	
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+    private ISymptomReader symptomReader;
+	private ISymptomWriter symptomWriter;
+	
+	
+	// le constructeur qui va initialiser writer et reader
+	
+	public AnalyticsCounter (ISymptomReader reader, ISymptomWriter writer) {
+		this.symptomReader = reader;
+		this.symptomWriter = writer;
 	}
-}
+	
+	
+	//methode pour recuperer la liste des symptoms à partir de reader
+	
+	public List<String> getSymptoms(){
+	    return symptomReader.getSymptoms(); 
+	}
+	
+	
+	//méthode pour calculer les occurences de chaque symptome
+	
+	public Map<String, Integer> countSymptoms(List<String>symptoms){
+		Map<String, Integer> symptomCount = new HashMap<>();
+		
+		for(String symptom : symptoms) {
+			symptomCount.put(symptom, symptomCount.getOrDefault(symptom, 0)+1);
+		}
+	    return symptomCount;
+		
+	}
+	
+	
+	//méthode pour trier les symptomes par ordre alphabetique
+	
+	public Map<String, Integer> sortSymptoms(Map<String, Integer>symptoms) {
+		
+		//utilisation d'une treeMap pour un tri naturel des symptomes
+		
+		Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
+		return sortedSymptoms;
+	}
+	
+	
+	//méthode pour ecrire les symptomes dans le fichier de sortie
+	
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		symptomWriter.writeSymptoms(symptoms);
+	}
+	
+	
+	
+} 
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -14,25 +14,30 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
 		
 		
 		// The name of the file in which to write the symptoms
-		String myFile = "result.out";// pas besoin d extention
+		
+		String myFile = "result.out";
 		
 		
 		// Write the symptoms to result.out file with the BufferedWriter
+		
 		try(BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"))){
 		
 			
 			// Browse the Map to add each symptom and its quantity
+			
 			for(Map.Entry<String, Integer> entry : symptoms.entrySet()) {
 				String line = entry.getKey()+": "+entry.getValue();
 				writer.write(line);
+				//Skip the line
+				writer.newLine(); 
+				writer.newLine(); 
 				
-				writer.newLine();    //Skip the line
 			}
-			System.out.println("The symptoms are written in "+myFile);
+			System.out.println("The symptoms are written in the file:  "+""+myFile+"");
 			
-			
+		//Error handling in case of writing error	
 		}catch(IOException e) {
-			e.printStackTrace();    //Error handling in case of writing error
+			e.printStackTrace();    
 		}
 		
 		
